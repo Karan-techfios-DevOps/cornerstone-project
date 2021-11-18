874807a58e8e33b18dcdcff75d3e57390d808b52
patelkar92 <85907228+patelkar92@users.noreply.github.com>
2021-11-18 15:33:45 -0500

    Delete Sprint1-Solutions/Jenkins-jobs/Ex1-CloudFormationforInstallingDockeronEC2Solution directory

--- Sprint1-Solutions/Jenkins-jobs/Ex1-CloudFormationforInstallingDockeronEC2Solution/DockerSubnet.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_subnet" "docker-subnet" {
-  vpc_id     = aws_vpc.Docker-TF.id
-  cidr_block = "10.0.0.0/24"
-
-  tags = {
-    Name = "Docker_Subnet-TF"
-  }
-}
--- Sprint1-Solutions/Jenkins-jobs/Ex1-CloudFormationforInstallingDockeronEC2Solution/Readme.md
+++
@@ -1,2 +0,0 @@
-This is the terraform script for installing Docker on Amazon Linux Server and add dockeradmin user.
-
